fd694c3658f0eebddca372bcc7d7a212a4c35caf
Robério <rroberiosouza@gmail.com>
2024-06-27 22:05:41 -0300

    Implementado metodo de criação do banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,8 +16,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  _recuparBanco() async {
+    final caminhoBanco = await getDatabasesPath();
+    final localBanco = join(caminhoBanco, "banco.db");
+
+    var retorno =
+        await openDatabase(localBanco, version: 1, onCreate: (db, dbVersion) {
+      String sql =
+          "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER)";
+      db.execute(sql);
+    });
+    print("Aberto: " + retorno.isOpen.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
+    _recuparBanco();
     return const Placeholder();
   }
 }
